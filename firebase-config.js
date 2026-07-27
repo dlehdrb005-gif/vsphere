7ad0f2f1c2c67272bb83c5a8b971987da00b95d5
dlehdrb005-gif <274573264+dlehdrb005-gif@users.noreply.github.com>
2026-07-27 20:36:37 +0900

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,16 @@
 // Firebase Console > Project settings > Your apps > Web app config 값을 여기에 붙여 넣으세요.
 // Firebase 웹 설정값은 공개 식별자입니다. 비밀번호나 서비스 계정 키는 넣지 마세요.
 window.VSPHERE_FIREBASE_CONFIG = {
-  apiKey: "PASTE_FIREBASE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "PASTE_FIREBASE_APP_ID",
+  apiKey: "AIzaSyDXBFBqMIFL1WPG-i_l0Qw2Zu9PiabZS7g",
+  authDomain: "vsphere-b04a9.firebaseapp.com",
+  projectId: "vsphere-b04a9",
+  storageBucket: "vsphere-b04a9.firebasestorage.app",
+  messagingSenderId: "63935811024",
+  appId: "1:63935811024:web:57e6ce3e96dab8616c7dd5",
 };
 
 // 임시 관리자 판별용 이메일 목록입니다.
 // 실제 운영에서는 서버/Firebase Custom Claims로 관리자 권한을 고정하는 편이 안전합니다.
 window.VSPHERE_ADMIN_EMAILS = [
-  "admin@example.com",
+  "dlehdrb005@gmail.com",
 ];
